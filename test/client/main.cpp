dea2163cd34d978a97ee09b90d832f75d1890950
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-07-18 16:13:34 +0200

    update: show loglevels in test client

--- test/client/main.cpp
+++ test/client/main.cpp
@@ -6,4 +6,8 @@ int main() {
     tablog::Tablog* logger = &tablog::Tablog::getInstance();
     logger->configure("TestClient", true);
     logger->log(tablog::DEBUG, "Tablog");
+    logger->log(tablog::INFO, "Tablog");
+    logger->log(tablog::WARNING, "Tablog");
+    logger->log(tablog::ERROR, "Tablog");
+    logger->log(tablog::CRITICAL, "Tablog");
 }
