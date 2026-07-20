0385f640828c358be3f4238f8b738257467a041a
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-07-20 16:37:55 +0200

    update: test registry, #1

--- test/client/main.cpp
+++ test/client/main.cpp
@@ -11,10 +11,16 @@ int main() {
     logger->configure("TestClient", true);
     registry->registerLogger("TestClient", logger);
 
-    std::shared_ptr<tablog::Tablog> loggerInstance = registry->get("TestClient"); 
-    loggerInstance->log(tablog::DEBUG, "Tablog");
-    loggerInstance->log(tablog::INFO, "Tablog");
-    loggerInstance->log(tablog::WARNING, "Tablog");
-    loggerInstance->log(tablog::ERROR, "Tablog");
-    loggerInstance->log(tablog::CRITICAL, "Tablog");
+    std::shared_ptr<tablog::Tablog> logger2 = std::make_shared<tablog::Tablog>();
+    logger2->configure("TestClient2", true);
+    registry->registerLogger("TestClient2", logger2);
+
+
+    std::shared_ptr<tablog::Tablog> loggerInstance1 = registry->get("TestClient");
+    std::shared_ptr<tablog::Tablog> loggerInstance2 = registry->get("TestClient2");  
+    loggerInstance1->log(tablog::DEBUG, "Tablog");
+    loggerInstance2->log(tablog::INFO, "Tablog");
+    loggerInstance1->log(tablog::WARNING, "Tablog");
+    loggerInstance2->log(tablog::ERROR, "Tablog");
+    loggerInstance1->log(tablog::CRITICAL, "Tablog");
 }
