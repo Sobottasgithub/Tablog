ded1d9094b5a76c323be795b674ee3fa7514c950
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-07-20 16:38:30 +0200

    update: version 0.1.0

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
 
-      version = "0.0.1";
+      version = "0.1.0";
 
       commonDeps = with pkgs; [
         cmake
